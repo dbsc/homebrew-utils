eb1c3a53e7a67f48bd2738d068695d1395793064
André Dal Bosco <andredbsc@gmail.com>
2023-02-26 02:28:36 +0100

    fix manpages link

--- python.rb
+++ python.rb
@@ -3,7 +3,7 @@ module PythonSetupInstallMethods
     @formula.system @venv_root/"bin/python", "setup.py", "install", *args
   end
 
-  def python_setup_install_and_link(args: [])
+  def python_setup_install_and_link(args: [], link_manpages: true)
     bin_before = Dir[@venv_root/"bin/*"].to_set
     man_before = Dir[@venv_root/"share/man/man*/*"].to_set if link_manpages
 
@@ -12,8 +12,9 @@ module PythonSetupInstallMethods
     bin_after = Dir[@venv_root/"bin/*"].to_set
     bin_to_link = (bin_after - bin_before).to_a
     @formula.bin.install_symlink(bin_to_link)
+    return unless link_manpages
 
-    man_after = Dir[@venv_root/"share/man/man*/*"].to_set
+    man_after = Dir[@venv_root/"share/man/man*/*"].to_set if link_manpages
     man_to_link = (man_after - man_before).to_a
     man_to_link.each do |manpage|
       (@formula.man/Pathname.new(manpage).dirname.basename).install_symlink manpage
