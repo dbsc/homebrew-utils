ce9f941fec10d168b610fc819d43e94e4d718781
André Dal Bosco <andredbsc@gmail.com>
2023-03-02 15:56:59 +0100

    add Upscayl cask

---
+++ Casks/upscayl.rb
@@ -0,0 +1,9 @@
+cask "upscayl" do
+  version "2.0.1"
+  sha256 "13ccea5608c1e5bf88035b6614b86b00b910b6d7567a99a236b01ac10b0ef9a1"
+
+  url "https://github.com/upscayl/upscayl/releases/download/v2.0.1/upscayl-2.0.1-mac.dmg"
+  name "Upscayl"
+  desc "Open source AI image upscaler"
+  homepage "https://upscayl.github.io"
+end
