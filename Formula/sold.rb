07c3202f796134c55082eddffed170612a2aa9eb
André Dal Bosco <andredbsc@gmail.com>
2023-03-23 03:44:12 +0100

    add sold formula

---
+++ Formula/sold.rb
@@ -0,0 +1,46 @@
+class Sold < Formula
+  desc "Modern Linker"
+  # url "https://github.com/bluewhalesystems/sold.git", branch: "main"
+  head "https://github.com/bluewhalesystems/sold.git", branch: "main"
+
+  depends_on :macos
+  depends_on "cmake" => :build
+  depends_on "llvm" => :build if DevelopmentTools.clang_build_version <= 1200
+  depends_on "tbb"
+  depends_on "zstd"
+  depends_on "zlib"
+
+  fails_with :clang do
+    build 1200
+    cause "Requires C++20"
+  end
+
+  fails_with :gcc do
+    version "7"
+    cause "Requires C++20"
+  end
+
+  def install
+    ENV.llvm_clang if OS.mac? && (DevelopmentTools.clang_build_version <= 1200)
+
+    # Avoid embedding libdir in the binary.
+    # This helps make the bottle relocatable.
+    inreplace "common/config.h.in", "@CMAKE_INSTALL_FULL_LIBDIR@", ""
+    # Ensure we're using Homebrew-provided versions of these dependencies.
+    %w[mimalloc tbb zlib zstd].map { |dir| (buildpath/"third-party"/dir).rmtree }
+    args = %w[
+      -DCMAKE_BUILD_TYPE=Release
+      -DMOLD_LTO=ON
+      -DMOLD_USE_MIMALLOC=ON
+      -DMOLD_USE_SYSTEM_MIMALLOC=ON
+      -DMOLD_USE_SYSTEM_TBB=ON
+      -DCMAKE_SKIP_INSTALL_RULES=OFF
+    ]
+
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+
+    pkgshare.install "test"
+  end
+end
