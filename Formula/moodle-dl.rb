c53966ecb98019736bcb46ff074a8200b9c6d656
André Dal Bosco <andredbsc@gmail.com>
2023-04-22 01:40:24 +0200

    update moodle-dl

--- Formula/moodle-dl.rb
+++ Formula/moodle-dl.rb
@@ -1,3 +1,14 @@
-class MoodleDL < Formula
-  # TODO
+class MoodleDl < Formula
+  include Language::Python::Virtualenv
+
+  desc "Download course content fast from Moodle"
+  homepage "https://github.com/dbsc/moodle-dl"
+  license "GPL-3.0-or-later"
+  head "https://github.com/dbsc/moodle-dl.git", branch: "main"
+
+  depends_on "python@3.9"
+
+  def install
+    virtualenv_install_with_resources
+  end
 end
