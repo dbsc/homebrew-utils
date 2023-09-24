e1caf27254d724746935bb3e7ac325a61e0af299
André Dal Bosco <andredbsc@gmail.com>
2023-09-24 23:00:34 +0200

    add charon

---
+++ Formula/charon.rb
@@ -0,0 +1,26 @@
+class Charon < Formula
+    desc "Interface with the rustc compiler for the purpose of program verification"
+    homepage "https://github.com/AeneasVerif/charon"
+    url "https://github.com/AeneasVerif/charon/archive/refs/heads/main.zip"
+
+    depends_on "rustup"
+    depends_on "ocaml"
+    depends_on "opam"
+
+    def install
+      system "rustup", "install", "stable"
+      system "rustup", "default", "stable"
+      system "make", "build-charon-rust"
+
+      system "opam", "init", "-a"
+      system "opam", "switch", "create", "4.13.1+options"
+      system "opam", "install", "ppx_deriving", "visitors", "easy_logging", "zarith", "yojson", "core_unix", "odoc"
+      system "make", "build-charon-ml"
+
+      bin.install "bin/charon"
+    end
+
+    test do
+      system "make", "tests"
+    end
+  end
